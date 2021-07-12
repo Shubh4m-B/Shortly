3413b32ad34dce9ce95abcc7c96800f9aea8a3c9
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-07-12 20:23:31 +0530

    Add copy to clipboard functionality

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import '../Styles/Link.css'
 
 export default function Link(props) {
     const { link, short } = props
+    const [copied, setCopy] = useState(false)
+
+    const handleCopy = () => {
+        setCopy(true)
+    }
+
+    useEffect(() => {
+        setTimeout(() => setCopy(false), 1500)
+    }, [copied])
     return (
-        <div className="Link">
-            <span className="Link-long">{link}</span>
-            <div className="Link-short">
-                <span>{short}</span>
-                <button>Copy</button>
+        <CopyToClipboard text={short} onCopy={handleCopy}>
+            <div className="Link">
+                <span className="Link-long">{link}</span>
+                <div className="Link-short">
+                    <span>{short}</span>
+                    {!copied && <button>Copy</button>}
+                    {copied && <button id="Copied">Copied!</button>}
+                </div>
             </div>
-        </div>
+        </CopyToClipboard>
     )
 }
