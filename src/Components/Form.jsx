a4a483f2f2103b334cb7def8994811c82b7127ac
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-07-13 13:40:08 +0530

    Add style components to form component

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,95 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import styled from 'styled-components';
 import Link from './Link';
-import '../Styles/Form.css'
+
+
+const FormDiv = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    transform: translateY(-80px);
+
+    .Form {
+        width: 100%;
+        border-radius: 10px;
+        background-color: #3b3054;
+        background-repeat: no-repeat;
+        background-size: 110%;
+        background-position: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 40px 0;
+        margin-bottom: 5px;
+    }
+
+    .Form > form {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 100%;
+        margin: 10px 40px;
+    }
+
+    input {
+        width: 70%;
+        padding: 15px 25px;
+        font-size: 18px;
+        color: #919191;
+        outline: none;
+        border-radius: 5px;
+        border: none;
+    }
+
+    button {
+        font-family: "Poppins", sans-serif;
+        background-color: #2acfcf;
+        color: white;
+        font-weight: 500;
+        font-size: 18px;
+        outline: none;
+        border: none;
+        transition: all 0.3s ease-in-out;
+        width:20%;
+        border-radius: 5px;
+        padding: 12px 25px;
+        transition: all 0.3s ease-in-out;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #5ff3f3;
+    }
+
+    @media screen and (max-width: 1200px) {
+        .Form {
+            background-size: 300%;
+        }
+    }
+
+    @media screen and (max-width: 750px) {
+        .Form {
+            padding: 20px 0;
+        }
+        form {
+            background-size: 500%;
+            flex-direction: column;
+            width: 100%;
+        }
+        input {
+            width: 84%;
+        }
+        button {
+            width: 100%;
+            margin: 10px 0 0 0;
+            padding: 12px 25px;
+        }
+    }
+
+`
 
 export default function Form() {
     const BASE_URL = "https://api.shrtco.de/v2/shorten?url="
@@ -25,7 +113,7 @@ export default function Form() {
     }
 
     return (
-        <div className="Form-container">
+        <FormDiv>
             <div className="Form" style={{ backgroundImage: "url(/images/bg-shorten-desktop.svg)" }}>
                 <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Shorten a link here ... " value={link} onChange={handleChange} />
@@ -35,6 +123,6 @@ export default function Form() {
             {links.map((link) => (
                 <Link link={link.link} short={link.short} />
             ))}
-        </div>
+        </FormDiv>
     )
 }
